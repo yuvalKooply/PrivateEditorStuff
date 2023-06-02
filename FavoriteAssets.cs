d1e799e6dbbc96109203fa00365ca66013a3e701
yuvalKooply <97828418+yuvalKooply@users.noreply.github.com>
2023-06-03 01:25:59 +0300

    Favorite assets refactor + new features

--- FavoriteAssets.cs
+++ FavoriteAssets.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using Com.Kooply.Unity.ExtensionMethods;
 using UnityEditor;
 using UnityEngine;
@@ -24,8 +25,36 @@ namespace Editor.Private
             public string name;
             public string type;
         }
+        
+        
+        
+        [MenuItem("Window/Custom Panels/Favorites")]
+        public static void ShowWindow()
+        {
+            GetWindow<FavoriteAssets>("♥ Favorites");
+        }
+        
+        [MenuItem("Assets/♥")]
+        private static void AddToFavorites()
+        {
+            var instance = GetWindow<FavoriteAssets>();
+            if (instance)
+            {
+                foreach (var assetGUID in Selection.assetGUIDs)
+                    instance.PinAsset(assetGUID);
+            }
+            
+            ShowProjectPanel();
+        }
+        
+        private static void ShowProjectPanel()
+        {
+            EditorApplication.ExecuteMenuItem("Window/General/Project");
+        }
 
-        private static string GetPrefix() => Application.productName; 
+        private static string GetPrefix() => Application.productName + "_"; 
+        
+        
         
         [SerializeField]
         private DataWrapper assetsData;
@@ -41,90 +70,141 @@ namespace Editor.Private
         }
 
         private Vector2 _scrollView = Vector2.zero;
-
-        [MenuItem("Window/Custom Panels/Favorites")]
-        public static void ShowWindow()
-        {
-            GetWindow<FavoriteAssets>("♥ Favorites");
-        }
+        private bool _shouldShowMenu;
+        private int _focusedIndex = -1;
+        
 
         public void OnGUI() 
         {
-            GUILayout.BeginVertical(EditorStyles.helpBox);
-            if (GUILayout.Button("Pin Selected Assets", EditorStyles.miniButton))
+            if (assetsData.assets.Count > 0)
             {
-                foreach (var assetGUID in Selection.assetGUIDs)
+                GUILayout.BeginVertical();
+
+                if (!_shouldShowMenu)
                 {
-                    var assetData = new AssetData
+                    if (GUILayout.Button("Menu", EditorStyles.miniButton, GUILayout.Width(80), GUILayout.Height(20)))
+                        _shouldShowMenu = true;
+                }
+                else
+                {
+                    GUILayout.BeginHorizontal();
+                    if (GUILayout.Button("<", EditorStyles.miniButton, GUILayout.Width(80), GUILayout.Height(20)))
+                        _shouldShowMenu = false;
+                    
+                    var previousColor = GUI.backgroundColor;
+                    GUI.backgroundColor = Color.red;
+
+                    if (GUILayout.Button("Clear All", EditorStyles.miniButton, GUILayout.Width(80), GUILayout.Height(20)))
                     {
-                        guid = assetGUID,
-                        path = AssetDatabase.GUIDToAssetPath(assetGUID)
-                    };
+                        _focusedIndex = -1;
+                        _shouldShowMenu = false;
+                        assetsData.assets.Clear();
+                        SaveData();
+                    }
                     
-                    var asset = AssetDatabase.LoadAssetAtPath<Object>(assetData.path);
-                    assetData.name = asset.name;
-                    assetData.type = asset.GetType().ToString();
-                    assetsData.assets.Add(assetData);
+                    GUI.backgroundColor = previousColor;
+                    
+                    GUILayout.EndHorizontal();
                 }
-                SaveData();
-            }
-            GUILayout.EndVertical();
 
-            GUILayout.BeginHorizontal(EditorStyles.toolbar);
-            {
-                GUILayout.Label("Pinned Assets:");
-                if (GUILayout.Button("▼ Sort Assets", EditorStyles.toolbarButton))
-                    assetsData.assets.Sort(AssetDataComparer);
-            }
-            GUILayout.EndHorizontal();
+                GUILayout.EndVertical();
+            
+                _scrollView = GUILayout.BeginScrollView(_scrollView);
 
-            _scrollView = GUILayout.BeginScrollView(_scrollView);
-            foreach (var assetData in AssetsData.assets) 
-            {
-                GUILayout.BeginHorizontal();
-
-                if (GUILayout.Button(new GUIContent("Open", "Open file with default app"), GUILayout.ExpandWidth(false)))
+                var index = 0;
+                foreach (var assetData in AssetsData.assets)
                 {
-                    var extension = Path.GetExtension(assetData.path);
-                    
-                    if (!extension.Equals(".unity"))
-                        EditorUtility.OpenWithDefaultApp(assetData.path);
-                    else
-                        UnityEditor.SceneManagement.EditorSceneManager.OpenScene(assetData.path, UnityEditor.SceneManagement.OpenSceneMode.Single);
+                    GUILayout.BeginHorizontal();
+
+                    try
+                    {
+
+                        if (_focusedIndex == index)
+                        {
+                            if (GUILayout.Button(new GUIContent("X", "Remove"), GUILayout.ExpandWidth(false)))
+                            {
+                                RemovePin(assetData);
+                                break;
+                            }
+                        }
+
+                        if (GUILayout.Button(
+                                new GUIContent(" " + assetData.name, AssetDatabase.GetCachedIcon(assetData.path)),
+                                GUILayout.Height(18)))
+                        {
+                            var asset = AssetDatabase.LoadAssetAtPath<Object>(assetData.path);
+                            EditorGUIUtility.PingObject(asset);
+                            Selection.activeObject = asset;
+
+                            var extension = Path.GetExtension(assetData.path);
+                            if (!extension.IsNullOrEmpty())
+                            {
+                                AssetDatabase.OpenAsset(asset);
+                                ShowProjectPanel();
+                            }
+                            else if (AssetDatabase.IsValidFolder(assetData.path))
+                            {
+                                var projectTab = Type.GetType("UnityEditor.ProjectBrowser,UnityEditor");
+                                var instance = projectTab.GetField("s_LastInteractedProjectBrowser",
+                                    BindingFlags.Static | BindingFlags.Public).GetValue(null);
+                                var showDirMeth = projectTab.GetMethod("ShowFolderContents",
+                                    BindingFlags.NonPublic | BindingFlags.Instance);
+                                showDirMeth?.Invoke(instance, new object[] {asset.GetInstanceID(), true});
+
+                                ShowProjectPanel();
+                            }
+                        }
+
+                        if (_focusedIndex == index)
+                        {
+                            GUI.enabled = index > 0;
+                            if (GUILayout.Button(new GUIContent("▲", "Move up"), GUILayout.ExpandWidth(false)))
+                            {
+                                MoveAssetUp(index);
+                                break;
+                            }
+
+                            GUI.enabled = index < AssetsData.assets.Count - 1;
+
+                            if (GUILayout.Button(new GUIContent("▼", "Move down"), GUILayout.ExpandWidth(false)))
+                            {
+                                MoveAssetDown(index);
+                                break;
+                            }
+
+                            GUI.enabled = true;
+
+                            if (GUILayout.Button(new GUIContent("Done"), GUILayout.ExpandWidth(false)))
+                                _focusedIndex = -1;
+                        }
+                        else
+                        {
+                            if (GUILayout.Button(new GUIContent("Edit"), GUILayout.ExpandWidth(false)))
+                                _focusedIndex = index;
+                        }
+                    }
+                    finally
+                    {
+                        // Workaround to the "Invalid GUILayout state" error message
+                        index++;
+                        GUILayout.EndHorizontal();
+                    }
                 }
 
-                if (GUILayout.Button(new GUIContent("Ping", "Highlight asset on Project panel"), GUILayout.ExpandWidth(false)))
-                    EditorGUIUtility.PingObject(AssetDatabase.LoadAssetAtPath<Object>(assetData.path));
-
-                if (GUILayout.Button(new GUIContent(" " + assetData.name, AssetDatabase.GetCachedIcon(assetData.path)), GUILayout.Height(18)))
-                {
-                    var asset = AssetDatabase.LoadAssetAtPath<Object>(assetData.path);
-                    EditorGUIUtility.PingObject(asset);
-                    Selection.activeObject = asset;
-                    
-                    var extension = Path.GetExtension(assetData.path);
-                    if (!extension.IsNullOrEmpty() && extension!.Equals(".prefab"))
-                        AssetDatabase.OpenAsset(asset);
-                }
-
-                if (GUILayout.Button(new GUIContent("X", "Un-pin"), GUILayout.ExpandWidth(false))){
-                    RemovePin(assetData);
-                    break;
-                }
-
-                GUILayout.EndHorizontal();
+                GUILayout.EndScrollView();
             }
-            GUILayout.EndScrollView();
+            else
+                GUILayout.Label("No Favorites (right click an asset and select ♥)");
         }
 
-        private void SaveData ()
+        private void SaveData()
         {
             var key = GetPrefix() + "pinned";
             var json = JsonUtility.ToJson(AssetsData);
             EditorPrefs.SetString(key, json);
         }
 
-        private void LoadData ()
+        private void LoadData()
         {
             assetsData = new DataWrapper();
 
@@ -136,15 +216,49 @@ namespace Editor.Private
             }
         }
 
-        private void RemovePin (AssetData assetData)
+        private void PinAsset(string assetGuid)
+        {
+            var assetData = new AssetData
+            {
+                guid = assetGuid,
+                path = AssetDatabase.GUIDToAssetPath(assetGuid)
+            };
+                    
+            var asset = AssetDatabase.LoadAssetAtPath<Object>(assetData.path);
+            assetData.name = asset.name;
+            assetData.type = asset.GetType().ToString();
+            assetsData.assets.Add(assetData);
+        }
+
+        private void RemovePin(AssetData assetData)
         {
             assetsData.assets.Remove(assetData);
             SaveData();
+            _focusedIndex = -1;
         }
 
-        private int AssetDataComparer (AssetData left, AssetData right)
+        private void MoveAssetUp(int index)
         {
-            return left.type.CompareTo(right.type);
+            if (index <= 0)
+                return;
+
+            (assetsData.assets[index - 1], assetsData.assets[index]) = (assetsData.assets[index], assetsData.assets[index - 1]);
+            _focusedIndex--;
+        }
+        
+        private void MoveAssetDown(int index)
+        {
+            if (index >= assetsData.assets.Count)
+                return;
+
+            (assetsData.assets[index + 1], assetsData.assets[index]) = (assetsData.assets[index], assetsData.assets[index + 1]);
+            _focusedIndex++;
+        }
+
+        private void OnLostFocus()
+        {
+            _focusedIndex = -1;
+            _shouldShowMenu = false;
         }
     }
 }

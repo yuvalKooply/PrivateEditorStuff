90fccae8c16acc7179d618b808c73bb28cff5b97
yuvalKooply <yuval@kooply.com>
2025-01-10 19:28:19 +0200

    Aded RectTransform support for Copy Component

--- TransformCopyShortcuts.cs
+++ TransformCopyShortcuts.cs
@@ -9,6 +9,11 @@ namespace Editor
         private static Vector3 copiedPosition;
         private static Quaternion copiedRotation;
         private static Vector3 copiedScale;
+        private static Vector2 copiedAnchorMin;
+        private static Vector2 copiedAnchorMax;
+        private static Vector2 copiedPivot;
+        private static Vector2 copiedAnchoredPosition;
+        private static Vector2 copiedSizeDelta;
 
         [MenuItem("CONTEXT/Transform/Copy Component")]
         private static void CopyTransform(MenuCommand command)
@@ -17,6 +22,15 @@ namespace Editor
             copiedPosition = transform.position;
             copiedRotation = transform.rotation;
             copiedScale = transform.localScale;
+            
+            if (transform is RectTransform rectTransform)
+            {
+                copiedAnchorMin = rectTransform.anchorMin;
+                copiedAnchorMax = rectTransform.anchorMax;
+                copiedPivot = rectTransform.pivot;
+                copiedAnchoredPosition = rectTransform.anchoredPosition;
+                copiedSizeDelta = rectTransform.sizeDelta;
+            }
         }
 
         [MenuItem("CONTEXT/Transform/Paste Component")]
@@ -28,6 +42,16 @@ namespace Editor
             transform.position = copiedPosition;
             transform.rotation = copiedRotation;
             transform.localScale = copiedScale;
+            
+            if (transform is RectTransform rectTransform)
+            {
+                Undo.RecordObject(rectTransform, "Paste RectTransform Properties");
+                rectTransform.anchorMin = copiedAnchorMin;
+                rectTransform.anchorMax = copiedAnchorMax;
+                rectTransform.pivot = copiedPivot;
+                rectTransform.anchoredPosition = copiedAnchoredPosition;
+                rectTransform.sizeDelta = copiedSizeDelta;
+            }
         }
     }
 #endif
